d0ac9325dae88ce51c79a2fcad20fb31722b1535
Alexey Strakh <alexey.strakh@microsoft.com>
2019-07-15 12:24:51 -1000

    test collection view with the similar scenario

--- ListViewDynamicHeight.iOS/AppDelegate.cs
+++ ListViewDynamicHeight.iOS/AppDelegate.cs
@@ -23,6 +23,7 @@ namespace ListViewDynamicHeight.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             Xamarin.Calabash.Start();
+            global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
